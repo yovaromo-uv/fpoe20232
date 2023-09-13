b1559bf48427a49c3014e8b4762e1a54ca560a87
Yovany Romo <luis.romo@correounivalle.edu.co>
2023-09-13 18:19:33 -0500

    Update rectangulo

--- rectangulo.java
+++ rectangulo.java
@@ -1,4 +1,14 @@
 class Rectabgulo{
 	private float lado1;
-	private float lado2;
+	private float area;
+
+	public Rectabgulo(){
+	}
+
+	public Rectangulo(){
+	}
+
+	public void alcularRectangulo(){
+	}
+
 }
